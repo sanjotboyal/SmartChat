72d3af731b03bce80b8d9128f525022919949ff9
Cem Torun <t-cetoru@microsoft.com>
2020-07-29 17:04:53 -0400

    summary api working!

--- cognitive_services_pipeline/summarize.py
+++ cognitive_services_pipeline/summarize.py
@@ -1,16 +1,10 @@
 import requests
-import base64
 
-url = "https://proxy.api.deepaffects.com/text/generic/api/v1/async/summary"
-
-querystring = {"apikey":"<API_KEY>", "webhook":"<WEBHOOK_URL>"}
-
-{"summaryType": "abstractive", "summaryData": [{"speakerId":"spk", "text":"text blob for speaker"}]}
-
-headers = {
-    'Content-Type': "application/json",
-}
-
-response = requests.post(url, json=payload, headers=headers, params=querystring)
-
-print(response.text)
+r = requests.post(
+    "https://api.deepai.org/api/summarization",
+    files={
+        'text': open('sample.txt', 'rb'),
+    },
+    headers={'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'}
+)
+print(r.json())
