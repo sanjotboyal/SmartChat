5e1333b31110028d0a5b918da8f163a2ee79a5b9
Cem Torun <t-cetoru@microsoft.com>
2020-07-30 12:48:42 -0400

    sentiment api call works

--- Bot/bots/cognitive_services_pipeline/cog_ser_test.py
+++ Bot/bots/cognitive_services_pipeline/cog_ser_test.py
@@ -1,35 +1,39 @@
 import urllib.request
 import json
 from pprint import pprint
+import ast
 
-url = "https://eastus.api.cognitive.microsoft.com/text/analytics/v2.1/keyphrases?"
+def sentiment(input_content):
+  url = "https://eastus.api.cognitive.microsoft.com/text/analytics/v2.1/sentiment?"
 
-with open("sample.txt") as f:
-    content = f.read() # reads the content from sample.txt
+  # with open(input_content) as f:
+  #     content = f.read() # reads the content from sample.txt
+  #     # content = content.replace("\n","")
+  #     # print(content)
 
-values = {
-    "documents": [
-      {
-        "countryHint": "US",
-        "id": "1",
-        "text": content
-      }
-    ]
-  }
-
-headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-    'Ocp-Apim-Subscription-Key': '160923276c3b48e6a0215dac9b54e152'
+  values = {
+      "documents": [
+        {
+          "countryHint": "US",
+          "id": "1",
+          "text": input_content
+        }
+      ]
     }
 
-data = json.dumps(values).encode("utf-8")
-pprint(data)
+  headers = {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+      'Ocp-Apim-Subscription-Key': '160923276c3b48e6a0215dac9b54e152'
+      }
 
-try:
-    req = urllib.request.Request(url, data, headers)
-    with urllib.request.urlopen(req) as f:
-        res = f.read()
-    pprint(res.decode())
-except Exception as e:
-    pprint(e)
+  data = json.dumps(values).encode("utf-8")
+
+  try:
+      req = urllib.request.Request(url, data, headers)
+      with urllib.request.urlopen(req) as f:
+          res = f.read()
+      return ast.literal_eval(res.decode())["documents"][0]["score"]
+  except Exception as e:
+      return e
+
